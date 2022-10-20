0864512212d775d29bd4f0e84bf3f1593fe5f532
Mikael <dev.kov.mtl@gmail.com>
2022-10-20 12:14:26 -0400

    Added react-router-dom and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+
+import { Discover } from "./pages";
+
+const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="relative flex">
+      {/* Sidebar */}
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        {/* SearchBar */}
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            {/* Routes */}
+            <Routes>
+              <Route path="/" element={<Discover />} />
+            </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">{/* Top Play */}</div>
+        </div>
+      </div>
+      {/* Active Song */}
+      <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl">
+        {/* MusicPlayer */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
