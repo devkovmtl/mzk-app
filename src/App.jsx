ab315d4bc4f8617037a1656b7c4ec6e1ad4491ca
Mikael <dev.kov.mtl@gmail.com>
2022-10-20 13:14:07 -0400

    Some default component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Discover } from "./pages";
+import { Sidebar, Searchbar, TopPlay } from "./components";
 
 const App = () => {
   return (
     <div className="relative flex">
       {/* Sidebar */}
+      <Sidebar />
+
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         {/* SearchBar */}
+        <Searchbar />
+
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             {/* Routes */}
@@ -15,7 +20,9 @@ const App = () => {
               <Route path="/" element={<Discover />} />
             </Routes>
           </div>
-          <div className="xl:sticky relative top-0 h-fit">{/* Top Play */}</div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
+          </div>
         </div>
       </div>
       {/* Active Song */}
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Searchbar } from "./Searchbar";
+export { default as Sidebar } from "./Sidebar";
+export { default as TopPlay } from "./TopPlay";
