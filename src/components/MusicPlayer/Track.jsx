74368f27d4dcc78a07c106a9ffa2d6e527c99e8f
Mikael <dev.kov.mtl@gmail.com>
2022-10-21 10:16:17 -0400

    Music player - player track

---
+++ src/components/MusicPlayer/Track.jsx
@@ -0,0 +1,27 @@
+const Track = ({ isPlaying, isActive, activeSong }) => {
+  return (
+    <div className="flex-1 flex items-center justify-start">
+      <div
+        className={`${
+          isPlaying && isActive ? "animate-[spin_3s_linear_infinite]" : ""
+        } hidden sm:block h-16 w-16 mr-4`}
+      >
+        <img
+          src={activeSong?.images?.covertart}
+          alt="covert art"
+          className="rounded-full"
+        />
+      </div>
+      <div className="w-[50%]">
+        <p className="truncate text-white font-bold text-lg">
+          {activeSong?.title ? activeSong?.title : "No active Song"}
+        </p>
+        <p className="truncate text-gray-300">
+          {activeSong?.subtitle ? activeSong?.subtitle : "No active Song"}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Track;
