ee0cef51281b12682c6b782009f27684c6560d35
Mikael <dev.kov.mtl@gmail.com>
2022-10-22 14:06:43 -0400

    Default pages components and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import { Discover } from "./pages";
+import {
+  AroundYou,
+  ArtistDetails,
+  Discover,
+  Search,
+  SongDetails,
+  TopArtists,
+  TopCharts,
+} from "./pages";
 import { Sidebar, Searchbar, TopPlay, MusicPlayer } from "./components";
 
 const App = () => {
@@ -21,6 +29,12 @@ const App = () => {
             {/* Routes */}
             <Routes>
               <Route path="/" element={<Discover />} />
+              <Route path="/top-artists" element={<TopArtists />} />
+              <Route path="/top-charts" element={<TopCharts />} />
+              <Route path="/around-you" element={<AroundYou />} />
+              <Route path="/artists/:id" element={<ArtistDetails />} />
+              <Route path="/songs/:songid" element={<SongDetails />} />
+              <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
