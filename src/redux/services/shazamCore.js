1d86b993284044e3ac96352fe34fe9fdb7b5a7a2
Mikael <dev.kov.mtl@gmail.com>
2022-10-25 10:04:15 -0400

    Added endpoint to shazam api

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,6 +5,12 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: `https://shazam-core.p.rapidapi.com/v1`,
     prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "5811af7079mshd4b095df87626f4p190cf0jsna09415c110f0"
+      );
+      headers.set("X-RapidAPI-Host", "shazam-core.p.rapidapi.com");
+
       return headers;
     },
   }),
@@ -12,7 +18,10 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({
       query: () => "/charts/world",
     }),
+    getSongDetails: builder.query({
+      query: (songid) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
