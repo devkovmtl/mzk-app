96ea21c525cec3dda2827e0486667882828c0f02
Mikael <dev.kov.mtl@gmail.com>
2022-10-26 09:45:17 -0400

    shazam core api endpoints

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,17 +12,35 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({
       query: () => "/charts/world",
     }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
     getSongDetails: builder.query({
       query: (songid) => `/tracks/details?track_id=${songid}`,
     }),
     getSongRelated: builder.query({
       query: ({ songid }) => `/tracks/related?track_id=${songid}`,
     }),
+    getArtistDetails: builder.query({
+      query: ({ artistId }) => `/artists/details?artist_id${artistId}`,
+    }),
   }),
 });
 
+// useGetArtistDetailsQuery
 export const {
   useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
 } = shazamCoreApi;
